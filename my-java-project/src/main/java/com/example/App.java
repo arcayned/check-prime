22dac930793694d116b96466c1211a21b3e09a7d
arcayned <anika.greco@icloud.com>
2025-11-09 23:55:19 +0000

    [Feature] Implements prime number checking functionality

--- my-java-project/src/main/java/com/example/App.java
+++ my-java-project/src/main/java/com/example/App.java
@@ -1,10 +1,39 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class App {
     public static boolean isPrime(int number) {
-        return false;
+        if (number <= 1) {
+            return false;
+        }
+        boolean prime = false;
+        int i = 1;
+        int mod = 1;
+        while(mod!=0 && i < number) {
+            i = i + 1;
+            mod = number % i;
+        }
+        if (i >= number) {
+            prime = true;
+        }
+        else {
+            prime = false;
+        }
+        return prime;
     }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        boolean prime = false;
+        while (!prime) {
+            System.out.println("Insert a prime number to win");
+            int n = scanner.nextInt();
+            prime = isPrime(n);
+            if (prime == false) {
+                System.out.println("Wrong number, try again");
+            }
+        }
+        System.out.println("Congratulations, you found a prime number!");
     }
 }
